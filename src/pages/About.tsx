1ca9772338f970ce06ef1a634b80c55e5871920a
fragmepls <leo.oberrauch@gmail.com>
2025-10-02 15:28:33 +0200

    add intersection observer for fade-in effect on About page sections

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Link} from "react-router-dom";
 import "../styles/About.css";
+import {useIntersectionObserver} from "../hooks/useIntersectionObserver.ts";
 
 const About: React.FC = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const section1Ref = useRef<HTMLElement>(null);
+    const section2Ref = useRef<HTMLElement>(null);
+    const section3Ref = useRef<HTMLElement>(null);
+    const section4Ref = useRef<HTMLElement>(null);
+
+    useIntersectionObserver([section1Ref, section2Ref, section3Ref, section4Ref], {threshold: 0.5});
+
     return (
-        <div>
-            <h1>Welcome to the About Page</h1>
-            <p>This is your React + TypeScript about page.</p>
-            <Link to="/">Zur Home-Seite</Link>
+        <div ref={containerRef} className="home-container">
+            <section
+                className="fade-in-section first-section"
+                ref={section1Ref}
+            >
+                <h1>Welcome to the About Page</h1>
+                <p>This is your React + TypeScript about page.</p>
+                <Link to="/">Zur Home-Seite</Link>
+            </section>
         </div>
     );
 };
